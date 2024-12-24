b8c714b35169db55f10ee1f218544f9b84fb41b3
Minervamuses <93601732+Minervamuses@users.noreply.github.com>
2024-12-24 09:55:15 +0800

    Update final.py

--- final.py
+++ final.py
@@ -3,13 +3,13 @@ import easyocr
 import numpy as np
 
 # 設定影像路徑與 Haar Cascade 路徑
-image_path = "/mnt/c/Final/images/Cars2.png" # 替換為你的影像檔案路徑
-cascade_path = "/mnt/c/Final/haarcascade_russian_plate_number.xml"  # 替換為你的 XML 路徑
+image_path = "/mnt/c/Final/images/Cars54.png" # 我的環境為WSL(Linux)，替換成你執行環境下的路徑
+cascade_path = "/mnt/c/Final/haarcascade_russian_plate_number.xml"  # 同上
 
 # 讀取影像
 img = cv2.imread(image_path)
 if img is None:
-    print("影像讀取失敗，請確認影像路徑是否正確。")
+    print("影像讀取失敗。")
     exit()
 
 # 灰階處理
@@ -30,10 +30,10 @@ if len(car_plate_rects) > 0:
     # 顯示檢測到的車牌和裁切影像
     cv2.imshow("Detected Plate", img2)
     cv2.imshow("Cropped Plate", car_plate_img)
-    cv2.waitKey(0)  # 按任意鍵關閉視窗
+    cv2.waitKey(0)  
     #cv2.destroyAllWindows()
 else:
-    print("未檢測到車牌，請檢查影像或更改檢測參數。")
+    print("未檢測到車牌。")
     exit()
 
 # 放大車牌影像
@@ -43,15 +43,16 @@ height = int(car_plate_img.shape[0] * scale_percent / 100)
 dim = (width, height)
 resized_image = cv2.resize(car_plate_img, dim, interpolation=cv2.INTER_AREA)
 
-# 使用 EasyOCR 進行文字辨識
+# EasyOCR 
 reader = easyocr.Reader(["en"])  
 result = reader.readtext(resized_image)
 
 # 提取車牌號碼
 if result:
-    text = " ".join([item[1] for item in result])  # 提取 OCR 輸出的文字部分
-    text = text.replace(" ", "")  # 移除空白
+    text = " ".join([item[1] for item in result]) 
+    text = text.replace(" ", "") 
     print()
     print("車牌號碼:", text)
+    cv2.waitKey(0)  
 else:
     print("未能識別車牌號碼")
